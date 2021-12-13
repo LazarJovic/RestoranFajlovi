f204b5643212f49101b5149d44d6796a56b8db44
LazarJovic <lazar.13jovic@gmail.com>
2021-12-13 16:20:52 +0100

    Add exceptions and observer pattern classes

---
+++ src/observer/IzmenaTabeleEvent.java
@@ -0,0 +1,16 @@
+package observer;
+
+import java.util.EventObject;
+
+public class IzmenaTabeleEvent extends EventObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3925365893541549554L;
+
+	public IzmenaTabeleEvent() {
+		super("Izmena tabele");
+	}
+	
+}
---
+++ src/observer/Observer.java
@@ -0,0 +1,9 @@
+package observer;
+
+import java.util.EventObject;
+
+public interface Observer {
+
+	public void updatePerformed(EventObject e);
+	
+}
